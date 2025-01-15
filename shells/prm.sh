be1d32f77f615c184d00d581e5fcb60236d9240f
Dong237 <youxiang.dong@outlook.com>
2025-01-15 14:34:59 +0800

    update

--- shells/prm.sh
+++ shells/prm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CUDA_DEVICE_MAX_CONNECTIONS=1
-export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6
+export CUDA_VISIBLE_DEVICES=0,1,3,6
 DIR=`pwd`
 
 # Number of GPUs per GPU worker
@@ -18,11 +18,13 @@ MASTER_ADDR=${MASTER_ADDR:-localhost}
 # The port for communication
 MASTER_PORT=${MASTER_PORT:-6001}
 
-MODEL="/data/youxiang/huggingface/Qwen2.5-7B-Instruct" # "/data/tangbo/plms/Qwen2.5-7B-Instruct-GPTQ-Int8"
+MODEL="/data1/huggingface/Qwen2.5-7B-Instruct" 
 DATA="datasets/omegaPRM_v2/risk_reasoner_v2.jsonl"
-OUTPUT_DIR="models/RiskPRM_v2_full"
-DS_CONFIG_PATH="omegaPRM/prm/ds_config_zero3.json"
+OUTPUT_DIR="models/RiskPRM_v3_lora"
+DS_CONFIG_PATH="training/omegaPRM/prm/ds_config_zero2.json"
 WANDB_KEY="e28afd6154b7ecd865dde62fead55bba5994bc9a"
+
+EPOCHS=3
 
 DISTRIBUTED_ARGS="\
     --nproc_per_node $GPUS_PER_NODE \
@@ -36,22 +38,26 @@ echo "Distributed Arguments: $DISTRIBUTED_ARGS"
 
 
 # non-lazy processing of data + logging with tensorboard
-torchrun $DISTRIBUTED_ARGS omegaPRM/prm/step_level_qwen.py \
+torchrun $DISTRIBUTED_ARGS training/omegaPRM/prm/step_level_qwen.py \
     --model_name_or_path $MODEL \
+    --lazy_preprocess False \
+    --use_wandb False \
+    --key $WANDB_KEY \
+    --wandb_run_name "prm-lora-v2" \
     --hidden_dropout_prob 0.1 \
     --attention_probs_dropout_prob 0.1 \
     --data_path $DATA \
+    --validation_fraction 0.05 \
     --bf16 True \
     --output_dir $OUTPUT_DIR \
-    --num_train_epochs 3 \
+    --num_train_epochs $EPOCHS \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 4 \
-    --validation_fraction 0.1 \
     --logging_strategy "steps" \
-    --logging_steps 30 \
+    --logging_steps 20 \
     --eval_strategy "steps" \
-    --eval_steps 30 \
+    --eval_steps 20 \
     --save_strategy "steps" \
     --save_total_limit 2 \
     --learning_rate 1e-4 \
@@ -61,9 +67,8 @@ torchrun $DISTRIBUTED_ARGS omegaPRM/prm/step_level_qwen.py \
     --lr_scheduler_type "cosine" \
     --model_max_length 2048 \
     --gradient_checkpointing \
-    --deepspeed ${DS_CONFIG_PATH}
-    # --use_lora \
-    # --key $WANDB_KEY \
-    # --use_wandb True \
-    # --wandb_run_name "prm-run1" \
+    --deepspeed ${DS_CONFIG_PATH} \
+    --use_lora \
+    1> >(tee logs/prm/${EPOCHS}epochs.log) \
+    2> >(tee logs/prm/${EPOCHS}epochs.err >&2)
     
