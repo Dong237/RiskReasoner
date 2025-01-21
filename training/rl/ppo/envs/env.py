1e6bdfbdeeaeca1c128fd771ed719622e929eaf3
Dong237 <youxiang.dong@outlook.com>
2025-01-21 10:22:23 +0800

    fix termination condition

--- training/rl/ppo/envs/env.py
+++ training/rl/ppo/envs/env.py
@@ -2,7 +2,7 @@ import random
 import numpy as np
 from training.rl.ppo.models.prm import ProcessRM
 from utils.helper import load_dataset 
-from utils.constants import Prompts, SPLIT_TOKEN
+from utils.constants import Prompts, SPLIT_TOKEN, STEP_TAG
 
 
 class RiskEnv:
@@ -12,25 +12,37 @@ class RiskEnv:
         dataset_name, 
         dataset_path, 
         mode,
+        n_agents=1,
+        max_step=30,
         ):
         
         self.rank = rank
         self.mode = mode
         self.dataset_name = dataset_name
         self.dataset = load_dataset(dataset_path)
-        self.n_agents = 1
-        self.max_step = 10
+        self.n_agents = n_agents
+        self.max_step = max_step
         self.step_count = 0
-        
-        if self.mode == "test":
-            self.problem_idx = 0
         
         self.problem = None
         self.label = None
-        self.step_tag = "ки"
+        self.good_assessment = SPLIT_TOKEN + ": " + 'good'
+        self.bad_assessment = SPLIT_TOKEN + ": " + 'bad'
+        self.current_state_len = None
+        self.empirical_state_window = None  # A highly problem-specific attribute
+        self.step_tag = STEP_TAG
+        self.step_tag_real = "ки"
         self.current_state = None
-        
         self.instruction = Prompts.INSTRUCTION_STEP_BY_STEP.value
+        
+        # FIXME this setting is problem specific and needs adaptation in the future
+        # Windows are counts of characters in problem / resoning steps
+        self.forward_window = 3000
+        self.backward_window = 1000
+        # This is to make sure that when checking backwards, we do not check the forwrd window.
+        assert self.forward_window > self.backward_window, "forward_window should be larger than backward_window"
+        
+        self.last_few_steps = None
 
     def reset(self):
         self.set_seed(42)
@@ -40,21 +52,31 @@ class RiskEnv:
         self.label = data_dict["choices"][int(data_dict["gold"])]  # "good" or "bad"
         
         self.current_state = self.problem + "\n"  ## FIXME this needs adaptation
+        self.empirical_state_window = len(self.current_state) + self.forward_window # would be around 4000
         obs = np.array([self.current_state], dtype=np.object_)
         self.step_count = 0
         return obs
     
     def step(self, action):
         self.step_count += 1
-        
         # Get next state
-        action = action[0].replace(self.step_tag, "").strip()
-        self.current_state = self.current_state + action + " " + self.step_tag + "\n"
+        action_str = action[0]
+        self.current_state = self.current_state + action_str
+        self.current_state_len = len(self.current_state)
         next_obs = np.array([self.current_state], dtype=np.object_)
+        self.last_few_steps = self.current_state.replace(
+            self.problem, ""
+            )[-self.backward_window:].lower()
         
-        # FIXME need to account for the cases where the conclusion comes within the first few steps
-        if SPLIT_TOKEN.lower() in action.lower():
-            dones = np.ones((self.n_agents), dtype=bool)
+        if (
+            self.current_state_len > self.empirical_state_window # reasoning has been going on for a few steps
+            and (
+                self.good_assessment.lower() in self.last_few_steps
+                or
+                self.bad_assessment.lower() in self.last_few_steps
+            )
+        ):
+            dones = np.ones((self.n_agents), dtype=bool)        
         elif self.step_count >= self.max_step:
             dones = np.ones((self.n_agents), dtype=bool)
         else:
